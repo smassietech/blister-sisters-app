44199734cd37c3ea66eb5ce1ef2e6de50489a436
smassietech <shadmassie@gmail.com>
2026-04-05 20:43:10 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import App from './App.jsx'
 // THIS IS WHERE YOU PASTE YOUR FIREBASE CONFIG
 // REPLACE THE ENTIRE object below with the one you saved in your notebook!
 window.__firebase_config = JSON.stringify({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAqcSneqSSD0Dy6KTDYDIwvgV4nX_rCdNA",
+  authDomain: "blister-sisters.firebaseapp.com",
+  projectId: "blister-sisters",
+  storageBucket: "blister-sisters.firebasestorage.app",
+  messagingSenderId: "823849072123",
+  appId: "1:823849072123:web:04023c282e38cb5e4cc75e"
 });
 
 window.__app_id = "blister-sisters-prod";
